0cbd5ace90cdb2a6783558d7d13eeb9e70cf52d2
PabloBnv <bnvpablo@gmail.com>
2023-03-10 17:58:51 -0300

    Initial Commit

---
+++ BackEnd - Introducción Java/Intro_Java_Egg/src/intro_java_egg/Intro_Java_Egg.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intro_java_egg;
+
+/**
+ *
+ * @author ViejoHucha
+ */
+public class Intro_Java_Egg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
