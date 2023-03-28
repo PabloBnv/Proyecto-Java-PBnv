eea2bb742c51dd48ed81272dea09d701ab353599
PabloBnv <bnvpablo@gmail.com>
2023-03-28 13:14:45 -0300

    Comienzo POO | EGG | intro Java

---
+++ BackEnd - Java POO/Video_Ej_01_Mascota/src/main/java/pablobnv/video_ej_01_mascota/entidades/Mascota.java
@@ -0,0 +1,13 @@
+package pablobnv.video_ej_01_mascota.entidades;
+
+public class Mascota {
+
+    public String nombre;
+    public String apodo;
+    public String tipo; //Gato, Perro, Conejo, Loro, Carpincho
+    public String color;
+    public String raza;
+    public int edad;
+    public boolean cola;
+
+}
---
+++ BackEnd - Java POO/Video_Ej_01_Mascota/src/main/java/pablobnv/video_ej_01_mascota/entidades/Usuario.java
@@ -0,0 +1,12 @@
+package pablobnv.video_ej_01_mascota.entidades;
+
+import java.util.Date;
+
+public class Usuario {
+
+    public String nombre;
+    public String apellido;
+    public String pais;
+    public int dni;
+    public Date nacimiento;
+}
